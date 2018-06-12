2267ecd3d0eef4914f93c33abb7c378a571b6c0a
Davide Zanella <davide.zanella10@gmail.com>
2018-06-12 13:32:59 +0200

    Modified to support user input

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import platform
 
 from nowvideo import nowVideo
+from speedvideo import speedVideo
 
 if platform.system() == "Windows":
     vlc_dir = r"C:\Program Files (x86)\VideoLAN\VLC"
@@ -14,9 +15,24 @@ else: #maybe linux
     vlc_cmd = "vlc"
 
 
+url = input('Paste the link of the video that you want to see:\n')
 
-site = nowVideo()
-stream = site.getStreamUrl("http://nowvideo.club/video/o8da8onvqd3w")
+if "nowvideo" in url:
+    site = nowVideo()
+elif "speedvideo" in url:
+    site = speedVideo()
+
+else:
+    print("Link type not recognised or not supported!!")
+    exit(0)
 
 
+print("Treating it as a ", site.siteName, " link...")
+
+
+#stream = site.getStreamUrl("http://nowvideo.club/video/o8da8onvqd3w")
+
+#stream = site.getStreamUrl("https://speedvideo.net/embed-5cn7a578qmgz.html")
+
+stream = site.getStreamUrl(url)
 subprocess.Popen([vlc_cmd, stream])
