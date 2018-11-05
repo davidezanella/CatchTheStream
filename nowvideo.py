fe147a351450d7ad0c04e480099c97de4923e69c
Davide <davide.zanella10@gmail.com>
2018-11-05 23:12:13 +0100

    fix nowvideo

--- nowvideo.py
+++ nowvideo.py
@@ -26,7 +26,34 @@ class nowVideo(streamSite):
             }
         )
 
-        streamUrl = self.returnFirstReGroup('var player = new Clappr\.Player\({source: "(.*)",', r.text)
+
+        js_code = self.returnFirstReGroup("\d,'(.*)'\.", r.text)
+        elems = js_code.split('|')
+
+        eval_fn = self.returnFirstReGroup("<script>eval(.*)\n<\/script>", r.text)
+
+        url1_n = self.returnFirstReGroup("\/\/(\d+)-", eval_fn)
+        url2 = self.returnFirstReGroup("-(\d+)\.", eval_fn)
+        site_n = self.returnFirstReGroup("\.(\d+)\.", eval_fn)
+        domain_n = self.returnFirstReGroup("\.(\d+)\:", eval_fn)
+        port_n = self.returnFirstReGroup("\:(\d+)\/", eval_fn)
+        suburl1_n = self.returnFirstReGroup("\:\d+\/(\d+)\/", eval_fn)
+        suburl2_n = self.returnFirstReGroup("\:\d+\/\d+\/(\d+)\/", eval_fn)
+        file_n = 16
+
+        port = elems[int(port_n)]
+        suburl1 = elems[int(suburl1_n)]
+        suburl2 = elems[int(suburl2_n)]
+
+        url1 = elems[int(url1_n)]
+        site = elems[int(site_n)]
+        domain = elems[int(domain_n)]
+        file_name = elems[int(file_n)]      
+
+        if(url1 == ""):
+            url1 = url1_n  
+
+        streamUrl = "http://" + url1 + "-" + url2 + "." + site + "." + domain + ":" + port + "/" + suburl1 + "/" + suburl2 + "/" + file_name + ".mp4"
 
         return streamUrl
 
