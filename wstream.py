dedf09a28611f11f9dbd7c7c247838d74a38e499
Davide <davide.zanella10@gmail.com>
2020-10-05 00:47:17 +0200

    Fixed wstream url extraction #1

--- wstream.py
+++ wstream.py
@@ -1,20 +1,28 @@
 from streamsite import streamSite
 import requests
+import js2py
+
+
+headers = {
+    'User-Agent': 'My User Agent 1.0',
+}
 
 class wStream(streamSite):
     def __init__(self):
         streamSite.__init__(self, "wstream")
 
     def getStreamUrl(self, url):
-        r = requests.get(url)
+        r = requests.get(url, headers=headers)
         if(r.status_code != 200):
-            print("Error getting url")
+            print("Error getting url", r.status_code)
             return
 
-        codeUrl = self.returnFirstReGroup("\|http\|poster\|none\|preload\|.*\|(.*)\|sources\|Player\|new'\.split\('\|'\)\)\)", r.text)
-        baseUrl = self.returnFirstReGroup("stream\|\|fastcdn\|(.*)\|data", r.text)
 
-        streamUrl = "https://" + baseUrl + ".fastcdn.stream/" + codeUrl + "/v.mp4"
+        js_code = self.returnFirstReGroup("eval\((.*)\)", r.text)
+        
+        f = js2py.eval_js("function() {hola_player=function(a){return a.file;}; return eval(" + js_code + ");}")
 
+        streamUrl = f()
+        
         return streamUrl
 
